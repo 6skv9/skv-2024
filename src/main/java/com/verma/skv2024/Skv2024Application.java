aa8e20d6e6bdbf988c484fa12d53088cfd5ce20d
6skv9 <reachtoskv@gmail.com>
2025-02-03 23:03:10 +0530

    Pushing error forcefully

--- src/main/java/com/verma/skv2024/Skv2024Application.java
+++ src/main/java/com/verma/skv2024/Skv2024Application.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Skv2024Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("Spring Boot Application");
+		throw new Exception("");
 		SpringApplication.run(Skv2024Application.class, args);
 	}
 
