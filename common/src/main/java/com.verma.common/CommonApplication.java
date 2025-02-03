d86b11790502ee5f61766ce0515147ebb2aaf031
6skv9 <reachtoskv@gmail.com>
2025-02-03 12:56:41 +0530

    Multi module configuration

---
+++ common/src/main/java/com.verma.common/CommonApplication.java
@@ -0,0 +1,9 @@
+package com.verma.common;
+
+public class CommonApplication {
+    public static void main(String[] args) {
+        PrintHelloFromCommon var1 = new PrintHelloFromCommon();
+        var1.cmPrint();
+        System.out.println("Hello its Common Module");
+    }
+}
