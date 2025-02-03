d6d0b644496134729eaa6a472781cecde52196fd
6skv9 <reachtoskv@gmail.com>
2025-02-03 22:56:29 +0530

    Multi module configuration

--- src/main/java/com/verma/skv2024/Skv2024Application.java
+++ src/main/java/com/verma/skv2024/Skv2024Application.java
@@ -8,6 +8,7 @@ public class Skv2024Application {
 
 	public static void main(String[] args) {
 		System.out.println("Spring Boot Application");
+		System.out.println("One more Line");
 		SpringApplication.run(Skv2024Application.class, args);
 	}
 
